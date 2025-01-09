65ff72d1169802fcead1107102f540bad9b79418
Defflip <blackkoutsol@gmail.com>
2025-01-09 14:37:30 +0200

    Initial commit

---
+++ encryption.py
@@ -0,0 +1,14 @@
+import os
+import boto3
+import base58
+import base64
+from dotenv import load_dotenv
+load_dotenv('.env')
+
+# Add your encrypt/decrypt details here OMMITTED for security
+
+def encrypt_private_key(private_key: str) -> str:
+    # Add your encrypt details here OMMITTED for security
+
+def decrypt_private_key(encrypted_private_key: str) -> str:
+    # Add your decrypt details here OMMITTED for security
